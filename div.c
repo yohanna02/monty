d5904d00691b0859b70773e9b3d138db7363ba98
Yohanna Abana <yohanaabana02@gmail.com>
2023-12-13 23:33:26 +0100

    fixed dividing by zero

--- div.c
+++ div.c
@@ -23,7 +23,7 @@ void _div(stack_t **stack, unsigned int line_number)
         fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
     }
-    if ((*stack)->next->n == 0)
+    if ((*stack)->n == 0)
     {
       fprintf(stderr, "L%d: division by zero\n", line_number);
       exit(EXIT_FAILURE);
