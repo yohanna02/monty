28c9b54861e2c1d04380cc9e386835313f613268
Yohanna Abana <yohanaabana02@gmail.com>
2023-12-16 16:21:29 +0100

    fix error

--- pchar.c
+++ pchar.c
@@ -15,7 +15,7 @@ void pchar(stack_t **stack, unsigned int line_number)
             fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
             exit(EXIT_FAILURE);
     }
-    if ((*stack)->n < 0 && (*stack)->n > 127)
+    if ((*stack)->n < 0 || (*stack)->n > 127)
     {
         fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
         exit(EXIT_FAILURE);
